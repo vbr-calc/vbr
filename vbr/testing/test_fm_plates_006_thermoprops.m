5d59c857de8abb13de0c0055c34e07cda569109c
chrishavlin <chris.havlin@gmail.com>
2019-12-07 16:08:11 -0800

    adding_examples: added a thermoprops, solliq page.
    also added a wrapper for MaterialProperties called ThermodynamicProperties

---
+++ vbr/testing/test_fm_plates_006_thermoprops.m
@@ -0,0 +1,51 @@
+function TestResult = test_fm_plates_006_thermoprops()
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % TestResult = test_fm_plates_006_thermoprops()
+  %
+  % code check for ThermodynamicProps(), which calls MaterialProperties()
+  %
+  % Parameters
+  % ----------
+  % none
+  %
+  % Output
+  % ------
+  % TestResult   True if passed, False otherwise.
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  disp('    **** Running test_fm_plates_006_thermoprops ****')
+  TestResult=true;
+
+  z=transpose(linspace(0,400,100)*1e3);
+  P0=1e5;
+  Rho_o=3300*ones(numel(z),1);
+  Kc_o = 4.17*ones(numel(z),1); % thermal conductivity [W/m/K]
+  Cp_o = 1100*ones(numel(z),1); % heat capacity [J/kg/K]
+  dTdz_ad = 0.5*1e-3; % adiabatic gradient [K/m]
+  z_L=150*1e3;
+  T=1400*z/z_L;
+  T(z>z_L)=(z(z>z_L)-z_L) * dTdz_ad + max(T(z<=z_L));
+  T=T+273;
+  PropTypes= {'con';'P_dep';'T_dep';'PT_dep'};  
+  P1=zeros(numel(z),4);
+  P2=zeros(numel(z),4);
+
+  for iPro=1:numel(PropTypes)
+    PropType=PropTypes{iPro};
+
+    [P1(:,1),P1(:,2),P1(:,3),P1(:,4)] = ThermodynamicProps(Rho_o,Kc_o,Cp_o,T,z,P0,dTdz_ad,PropType);
+    [P2(:,1),P2(:,2),P2(:,3),P2(:,4)] = MaterialProperties(Rho_o,Kc_o,Cp_o,T,z,P0,dTdz_ad,PropType);
+% [Rho,Cp,Kc,P]
+    diff=sum(abs(P1(:)-P2(:))./P1(:));
+    if diff > 0
+      TestResult=false;
+      disp('        ThermodynamicProps not matching MaterialProperties')
+    elseif sum(isnan(P1))>0
+      TestResult=false;
+      disp('        ThermodynamicProps solution contains nans')
+    elseif sum(isnan(P2))>0
+      TestResult=false;
+      disp('        MaterialProperties solution contains nans')
+    end
+  end
+
+end
