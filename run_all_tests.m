8315e443fdcdc257dc74763c07299efb81521c83
chrishavlin <chris.havlin@gmail.com>
2022-10-28 14:45:51 -0500

    update main test runner

--- run_all_tests.m
+++ run_all_tests.m
@@ -1,3 +1,11 @@
-cd vbr
-cd testing
-run_tests();
+
+vbr_init
+addpath('vbr/testing')
+
+TestResults = run_tests();
+
+if TestResults.failedCount > 0
+    error('At least one test failed, check log.')
+elseif TestResults.n_tests == 0
+    error('No tests collected.')
+end
