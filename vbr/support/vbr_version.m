6bed5807cdb51bd19c2dedad962b5ff395c166b7
chrishavlin <chris.havlin@gmail.com>
2023-10-25 17:17:48 -0500

    bump version, add release notes

--- vbr/support/vbr_version.m
+++ vbr/support/vbr_version.m
@@ -1,8 +1,8 @@
 function Version = vbr_version()
     % return the current VBRc version
     Version.major = 1;
-    Version.minor = 0;
-    Version.patch = 1;
+    Version.minor = 1;
+    Version.patch = 0;
     Version.version = [num2str(Version.major), '.', ...
                        num2str(Version.minor), '.', ...
                        num2str(Version.patch)];
