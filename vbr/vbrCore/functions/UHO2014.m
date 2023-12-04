ddd8bbf6422835a57e14642fcebbfe480808b551
sammykojo <151546165+sammykojo@users.noreply.github.com>
2023-12-04 16:21:11 -0500

    Update UHO2014.m
    State Variable in fix for UHO2014 (addition of units as subscript to Pressure & Temperature variable names )

--- vbr/vbrCore/functions/UHO2014.m
+++ vbr/vbrCore/functions/UHO2014.m
@@ -17,9 +17,9 @@ function [ VBR ] = UHO2014( VBR )
 
   % read in eletric parameters
   ele = VBR.in.electric.UHO2014_ol;
-  T = VBR.in.SV.T; % K (Temperature)
+  T = VBR.in.SV.T_K; % K (Temperature)
   Ch2o = VBR.in.SV.Ch2o; % ppm (water content)
-  P = VBR.in.SV.P; % GPa (Pressure)
+  P = VBR.in.SV.P_GPa * 1e9; % Pa (Pressure)
   
   % Vacancy Conduction
     Sv = ele.S_v; % S/m
