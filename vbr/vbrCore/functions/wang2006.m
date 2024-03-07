d62d06c49079626a663197c86c828ac4914d8f5f
sammykojo <151546165+sammykojo@users.noreply.github.com>
2024-03-07 13:23:41 -0500

    Update wang2006.m

--- vbr/vbrCore/functions/wang2006.m
+++ vbr/vbrCore/functions/wang2006.m
@@ -19,6 +19,8 @@ function [ VBR ] = wang2006( VBR )
   T = VBR.in.SV.T_K; % K (Temperature)
   Ch2o = VBR.in.SV.Ch2o; % ppm (water content)
   P = VBR.in.SV.P_GPa * 1e9; % Pa (Pressure)
+  Ch2o = Ch2o./1d4; % ppm => wtf
+
   
     % Hydrous Conduction
     SH = ele.S_H; % S/m
@@ -33,7 +35,7 @@ function [ VBR ] = wang2006( VBR )
     Va_A = ele.Va_A; % cc/mol
     HA = ele.H_A + Va_A.*P; % kJ
 
-  % calculate arrhenius relation for each conduction mechanism
+  % calculate Arrhenius relation for each conduction mechanism
   esig_A = arrh_dry(SA,HA,R,T);
   esig_H = arrh_wet(SH,HH,R,T,Ch2o,a,r);
   
@@ -50,10 +52,10 @@ end
 
 function sig = arrh_dry(S,H,k,T)
     exponent = -(H)./(k.*T);
-    sig = (10^S).*exp(exponent);
+    sig = (S).*exp(exponent);
 end
 
 function sig = arrh_wet(S,H,k,T,w,a,r)
  exponent = -(H-a.*(w.^(1/3)))./(k.*T);
-    sig = (10^S).*(w.^r).*exp(exponent);
+    sig = (S).*(w.^r).*exp(exponent);
 end
