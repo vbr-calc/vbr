87a980f604a5aa763a6dc886e2baeb034262079d
sammykojo <sammykojo@gmail.com>
2024-04-26 17:03:09 -0400

    updates 4

--- vbr/vbrCore/params/Params_Electric.m
+++ vbr/vbrCore/params/Params_Electric.m
@@ -3,7 +3,7 @@ function [params] = Params_Electric(method,GlobalParams)
   %
   % params = Params_Electric(method)
   %
-  % loads the parameters for an elastic method
+  % loads the parameters for an electric method
   %
   % Parameters:
   % ----------
@@ -15,14 +15,14 @@ function [params] = Params_Electric(method,GlobalParams)
   % ------
   % params    the parameter structure for the electric method
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  params.possible_methods={'yosh2009','SEO3','poe2010','wang2006','UHO2014','jones2012'};
+  params.possible_methods={'yosh2009_ol','SEO3_ol','poe2010_ol','wang2006_ol','UHO2014_ol','jones2012_ol','sifre2014_melt','ni2011_melt','gail2008_melt','HS1962'};
 
   if ~exist('GlobalParams')
     GlobalParams = Params_Global();
   end
 
-  if strcmp(method,'yosh2009')
-    params.func_name='yosh2009'; % the name of the Matlab function
+  if strcmp(method,'yosh2009_ol')
+    params.func_name='ec_yosh2009'; % the name of the Matlab function
     % Yoshino et al, 2009
     % Ionic Conduction
     params.S_i = 10^4.73; % S/m
@@ -48,8 +48,8 @@ function [params] = Params_Electric(method,GlobalParams)
     params.citations={'Yoshino et al. (2009), "The effect of water on the electrical conductivity of olivine aggregates and its implications for the electrical structure of the upper mantle", Earth and Planetary Science Letters, Volume 288, Issue 1-2, https://doi.org/10.1016/j.epsl.2009.09.032'};
     params.description='Yoshino (or alpha) reconciliation on H for hydrous Olivine conductivities (includes i & h conduction)';
 
-  elseif strcmp(method,'SEO3')
-    params.func_name='SEO3'; % the name of the matlab function
+  elseif strcmp(method,'SEO3_ol')
+    params.func_name='ec_SEO3'; % the name of the matlab function
     % Constable, 2006
     params.e = 1.602e-19; % C
     params.k = 8.617e-5; % eV/(mol*K)
@@ -74,8 +74,8 @@ function [params] = Params_Electric(method,GlobalParams)
     params.citations={'Constable (2009), "SEO3: A new model of olivine electrical conductivity", Geophysical Journal International, Volume 166, Issue 1, https://doi.org/10.1111/j.1365-246X.2006.03041.x'};
     params.description='Constable 2006 Standard Electrical Olivine Model (Anhydrous)';
 
-  elseif strcmp(method,'poe2010')
-    params.func_name='poe2010'; % the name of the Matlab function
+  elseif strcmp(method,'poe2010_ol')
+    params.func_name='ec_poe2010'; % the name of the Matlab function
     % Poe et al, 2010
     % hydrous 100 axis
     params.S_H100 = 10^2.59; % S/m   
@@ -119,8 +119,8 @@ function [params] = Params_Electric(method,GlobalParams)
     params.citations={'Poe et al. (2010), "Electrical conductivity anisotropy of dry and hydrous olivine at 8 GPa", Physics of Earth and Planetary Interiors, Volume 181, Issues 3–4, https://doi.org/10.1016/j.pepi.2010.05.003'};
     params.description='Poe et al 2010 Ol Conductivity for 3 crystal axes (hydrous & anhydrous), Parameters Sigma_0 and alpha are for wt% from Jones et al., 2012';
   
-   elseif strcmp(method,'wang2006')
-    params.func_name='wang2006'; % the name of the Matlab function
+   elseif strcmp(method,'wang2006_ol')
+    params.func_name='ec_wang2006'; % the name of the Matlab function
     % Wang, 2006
     params.S_H = 10^3.0; % S/m   
     params.H_H = 87; % kJ/mol
@@ -137,8 +137,8 @@ function [params] = Params_Electric(method,GlobalParams)
     params.citations={'Wang et al. (2006), "The effect of water on the electrical conductivity of olivine", Nature, Volume 443, doi:10.1038/nature05256'};
     params.description='Wang et al. conductivity of Ol aggregate (hydrous and Anhydrous)';
     
-   elseif strcmp(method,'UHO2014')
-    params.func_name='UHO2014'; % the name of the Matlab function
+   elseif strcmp(method,'UHO2014_ol')
+    params.func_name='ec_UHO2014'; % the name of the Matlab function
     % UHO, 2014
     params.H_v = 239; % kJ/mol
     params.S_v = 10^5.07; % S/m
@@ -161,20 +161,77 @@ function [params] = Params_Electric(method,GlobalParams)
         'conductivity law", [GeoChemistry, Geophysics, Geosystems], Volume 15, Issue 12, doi:10.1002/2014GC005496'};
     params.description='Gardes et al. derives universal Ol conductivity law based on laboratory database (tested against true petrophysical data) ';
 
-  elseif strcmp(method,'jones2012')
-    params.func_name='jones2012'; % the name of the Matlab function
+  elseif strcmp(method,'jones2012_ol')
+    params.func_name='ec_jones2012'; % the name of the Matlab function
 
     % Jones et al., 2012 (Hydrous)
-    params.jones2012_ol.S = 10.^(3.05); % S/m
-    params.jones2012_ol.r = 0.86; % unitless
-    params.jones2012_ol.H = 0.91; % eV
-    params.jones2012_ol.a = 0.09; % unitless
-    params.jones2012_ol.Va = 0; % cc/mol (activation volume)
-    params.jones2012_ol.k = 8.617e-5; % eV/(mol*K)
+    params.S = 10.^(3.05); % S/m
+    params.r = 0.86; % unitless
+    params.H = 0.91; % eV
+    params.a = 0.09; % unitless
+    params.Va = 0; % cc/mol (activation volume)
+    params.k = 8.617e-5; % eV/(mol*K)
 
     params.citations={'Jones et al. (2012), "Water in cratonic lithosphere: Calibrating laboratory- determined models ..."' ...
         'Geochemistry, Geophysics, Geosystems, Volume 13, http://dx.doi.org/10.1029/2012GC004055'};
     params.description='Jones et al. calibration of hydrous electrical conductivity from previous laboratory experiments to South African Jagersfontein and Gibeon Xenolith in situ, Constable 2006(SEO3) used as the anhydrous component';
-end
+
+  elseif strcmp(method,'sifre2014_melt')
+    params.func_name='ec_sifre2014'; % the name of the Matlab function
+
+    % Sifre et al., 2014 
+    % Partition coefficient
+    params.D = 0.006;
+
+    % H2O melt
+    params.h2o_a = 88774;
+    params.h2o_b = 0.3880;
+    params.h2o_c = 73029;
+    params.h2o_d = 4.54e-5;
+    params.h2o_e = 5.5607;
+
+     % C2O melt
+    params.c2o_a = 789166;
+    params.c2o_b = 0.1808;
+    params.c2o_c = 32820;
+    params.c2o_d = 5.50e-5;
+    params.c2o_e = 5.7956;
+
+    params.citations={'Sifre et al. (2014), "Electrical conductivity during incipient melting in the oceanic low-velocity zone"' ...
+        'Nature, Volume 509, https://doi.org/10.1038/nature13245'};
+    params.description='Sifre et al. parameterization of electrical conductivity in peridotite melt from volatile content in the incipient melt';
+
+   elseif strcmp(method,'ni2011_melt')
+    params.func_name='ec_ni2011'; % the name of the Matlab function
+
+    % Ni et al., 2011
+    params.Tcorr = 1146.8; % K, Temperature correction
+
+    params.citations={'Ni et al. (2011), "Electrical conductivity of hydrous basaltic melts: implications for partial melting in the upper mantle"' ...
+        'Contrib Mineral Petrol, Volume 162, https://doi.org/10.1007/s00410-011-0617-4'};
+    params.description='Ni et al. estimation of H2O volatile content and melt fraction of basaltic melt for electrical conductivity in LVZ';
+
+  elseif strcmp(method,'gail2008_melt')
+    params.func_name='ec_gail2008'; % the name of the Matlab function
+
+    %Gaillard et al., 2008
+    params.S = 3440; % S/m
+    params.H = 31.9; % KJ
+    params.R = 0.008314; % KJ /(K*mol)
+
+    params.citations={'Gaillard et al. (2008), "Carbonatite Melts and Electrical magmas' ...
+        ' is a few hundred parts per million by weight (ppmw) in mid-ocean ridge basalts (MORBs) Conductivity in the Asthenosphere"' ...
+        'Science, Volume 322, https://doi.org/10.1126/science.1164446'};
+    params.description='Gaillard et al. explaination of oceanic asthenosphere conductivity by carbonatite present in melt';
+
+    elseif strcmp(method,'HS1962')
+    params.func_name='ec_HS1962'; % the name of the Matlab function
+
+    % Hashin & Shtricktman et al., 1962 (Cited from Glover, 2010)
+
+    params.citations={'Glover (2010), "A generalized Archie’s law for n phases' ...
+        'Society of Exploration Geophysics, Volume 75, Issue 6, https://doi.org/10.1190/1.3509781'};
+    params.description='Glover derivation of Hashin-Stricktman (1962) for effective medium considerations';
+  end
   
 end
