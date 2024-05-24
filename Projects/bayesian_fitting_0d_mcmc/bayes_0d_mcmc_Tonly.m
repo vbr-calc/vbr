3600c5b7e48e4bd42d20efd4b3a0c520ef8908ac
chrishavlin <chris.havlin@gmail.com>
2024-05-24 15:39:56 -0500

    clean up, starting readme

--- Projects/bayesian_fitting_0d_mcmc/bayes_0d_mcmc_Tonly.m
+++ Projects/bayesian_fitting_0d_mcmc/bayes_0d_mcmc_Tonly.m
@@ -4,24 +4,39 @@ addpath(path_to_top_level_vbr)
 addpath('bayes_0d_funcs')
 vbr_init
 
+% prior distribution settings
+priors.T_K_mean = 1200 + 273;  % mean of T_K prior distribution
+priors.T_K_std = 200;  % standard deviation of T_K prior distribution
+
+% mcmc settings:
+% for testing, the following are set to small numbers. they should be increased,
+% which will be obvious when you run this with small numbers...
+settings.max_mcmc_iters = 100; % max iterations for this chain
+settings.mcmc_info_very_N = 10; % print info every N steps
+settings.mcmc_burnin_iters = round(0.2 * settings.max_mcmc_iters); % burn in iterations
+settings.mcmc_jump_std = priors.T_K_std * .05; % the jump magnitude for updating T_K
+settings.mcmc_initial_T_K = 0; % set to 0 to draw initial guess from distribution
+settings.mcmc_initial_guess_jump_std = priors.T_K_std; % jump magnitude for the initial guess
+settings.mcmc_acceptance_sc = 1; % acceptance threshold = sc * rand()
+
+% set the fixed state variables and single anelastic method
 settings.fixed_SVs.P_GPa = 2;
 settings.fixed_SVs.sig_MPa = 0.1;
 settings.fixed_SVs.phi = 0;
 settings.fixed_SVs.dg_um = 0.01 * 1e6;
 settings.fixed_SVs.f = 1. / 50.;
+settings.fit_a_fixed_TK = 1; % 1 to fixed hidden T_K, 0 to draw from distribution about a hidden mean
 settings.anelastic_method = 'eburgers_psp';
 
-max_mcmc_iters = 1000;
-mcmc_burnin_iters = round(0.2 * max_mcmc_iters);
-
-priors.T_K_mean = 1200 + 273;
-priors.T_K_std = 200;
-mcmc_jump_std = priors.T_K_std * .05;
-
-input_data = get_data(settings);  % the synthetic data we want to fit
+% get the synthetic data we want to fit
+input_data = get_data(settings);  % includes a hidden T_K value ...
 
 % make an initial draw for the modeled temperature, calculate
-current_model.T_K = draw_temperature(1, priors.T_K_mean, priors.T_K_std*2);
+if settings.mcmc_initial_T_K == 0
+    current_model.T_K = draw_temperature(1, priors.T_K_mean, settings.mcmc_initial_guess_jump_std);
+else
+    current_model.T_K = settings.mcmc_initial_T_K;
+end
 current_model = update_model_predictions(current_model, settings);
 current_model = update_likelihood(current_model, input_data);
 current_model = update_prior_probability(current_model, priors);
@@ -41,31 +56,49 @@ disp("  with predictions:")
 disp(["   Vs = ", num2str(current_model.Vs)])
 disp(["   Q = ", num2str(current_model.Q)])
 
-
-mcmc_samples_T_K = zeros(max_mcmc_iters,1);
+% start the itreations
+results.mcmc_samples_T_K = zeros(settings.max_mcmc_iters,1);
 mcmc_sample_i = 1;
 current_iter = 1;
-while current_iter <= max_mcmc_iters
 
-    if rem(current_iter, 100) == 0
-        disp(["mcmc step ", num2str(current_iter)])
-        disp(current_model.T_K)
+disp("")
+disp("Starting chain iterations")
+disp("")
+
+while current_iter <= settings.max_mcmc_iters
+
+    if rem(current_iter, settings.mcmc_info_very_N) == 0
+        if current_iter >= settings.mcmc_burnin_iters + 1
+            mean_T = mean(results.mcmc_samples_T_K(settings.mcmc_burnin_iters:current_iter));
+            msg = ["    mcmc step ", num2str(current_iter), ...
+                   ", model T_K=", num2str(current_model.T_K), ...
+                   "  mean T_K (after burn-in) = ", num2str(mean_T)];
+        else
+            msg = ["    mcmc step ", num2str(current_iter), ...
+                   ", model T_K=", num2str(current_model.T_K)];
+        end
+        disp(msg)
+
     end
-    % get a new model near the old by sampling from normal distribution near
+    % mcmc step 1: get a new model near the old by sampling from normal distribution near
     % current T_K guess
-    new_model.T_K = draw_temperature(1, current_model.T_K, mcmc_jump_std);
+    new_model.T_K = draw_temperature(1, current_model.T_K, settings.mcmc_jump_std);
 
-    % update stats of new model
+    % mcmc step 2: update model predictions, update the likelihood, prior and posterior
     new_model = update_model_predictions(new_model, settings);
     new_model = update_likelihood(new_model, input_data);
     new_model = update_prior_probability(new_model, priors);
     new_model = update_posterior(new_model);
 
-    % check model acceptence
-    accept_threshold = rand();
+    % check model acceptance
+    accept_threshold = settings.mcmc_acceptance_sc * rand();
 
+    % note: due to log scaling of Q, can have identically 0 probabilities for
+    % some values. could improve this by computing probabilities in log space,
+    % but we can just check for those special cases.
     if current_model.p_T_Vs_Q > 0 && new_model.p_T_Vs_Q > 0
-        accept_criteria = new_model.p_VsQ_T / (current_model.p_VsQ_T);
+        % the standard metropolis-hastings criteria
+        accept_criteria = new_model.p_VsQ_T / current_model.p_VsQ_T;
     elseif current_model.p_T_Vs_Q == 0 && new_model.p_T_Vs_Q > 0
         % take the new
         accept_criteria = 1.0;
@@ -73,43 +106,52 @@ while current_iter <= max_mcmc_iters
         % take the old
         accept_criteria = 0.0;
     else
-%        disp("both 0, yikes")
+        % both have 0 probability, flip a coin.
         accept_criteria = 0.5;
     end
 
+    % compare to the random acceptance threshold
     if accept_criteria > accept_threshold
         current_model = new_model;
     end
 
-%    if current_iter > mcmc_burnin_iters
-    mcmc_samples_T_K(mcmc_sample_i) = current_model.T_K;
+    % store current model temperature: note that this includes the burn-in
+    % iterations, which should be removed for the final analysis.
+    results.mcmc_samples_T_K(mcmc_sample_i) = current_model.T_K;
     mcmc_sample_i = mcmc_sample_i + 1;
-%    end
-
     current_iter = current_iter + 1;
 end
 
 % pull out the after_burn for analysis
-after_burn = mcmc_samples_T_K(mcmc_burnin_iters:end);
-x_after = 1:numel(after_burn);
-x_after = x_after+mcmc_burnin_iters-1 ;
+results.after_burn = results.mcmc_samples_T_K(settings.mcmc_burnin_iters:end);
+results.after_iters = 1:numel(results.after_burn);
+results.after_iters = results.after_iters+settings.mcmc_burnin_iters-1 ;
+results.mcmc_sample_i = mcmc_sample_i;
 
-
+% plot the result
 subplot(1,2,1)
-plot(mcmc_samples_T_K,'b')  % full
-hold on
-plot(x_after, after_burn, 'r') % just after burn
-plot([1, mcmc_sample_i],[input_data.T_mean, input_data.T_mean],'--k')
+% first plot the full series
+plot(results.mcmc_samples_T_K)  % full
+hold all
+% plot only the iterations after the burn in
+plot(results.after_iters, results.after_burn)
+% add a line for the actual input T
+plot([1, results.mcmc_sample_i],[input_data.T_mean, input_data.T_mean],'--k')
+xlabel("iterations")
+ylabel("T_K")
 
 subplot(1,2,2)
-hist(after_burn)
-title(mean(after_burn))
+hist(results.after_burn)
+results.mean_T_K = mean(results.after_burn);
+results.final_error = abs(results.mean_T_K - input_data.T_mean)./input_data.T_mean;
+ttl_str = ["mean T_K = ", num2str(mean(results.after_burn)), ...
+           " , error = ", num2str(results.final_error)];
+title(ttl_str)
 
+disp('')
 disp('final mean:')
-disp(['   T_K = ', num2str(mean(after_burn))])
+disp(['   T_K = ', num2str(results.mean_T_K)])
 disp('actual:')
 disp(['   T_K_input = ', num2str(input_data.T_mean)])
-
-
-
-
+disp('relative error:')
+disp(['   abs(T_K_input-T_K) / T_K_input = ', num2str(results.final_error)])
--- Projects/bayesian_fitting_0d_mcmc/readme.md
+++ Projects/bayesian_fitting_0d_mcmc/readme.md
@@ -1,21 +1,62 @@
 ## `bayesian_fiting_0d_mcmc` 
 
-A single paramter bayesian marcov-chain monte carlo (mcmc) simulation with 
+A single-parameter bayesian markov-chain monte carlo (mcmc) simulation with 
 Metropolis-Hastings sampling to invert a spot measurements of shear wave velocity 
 and attenuation for the underyling temperature. 
+
+### A brief overview of MCMC + Metropolis-Hastings
+
+insert notes 
+
+### Overview of the code 
+
+```matlab
+% prior distribution settings
+priors.T_K_mean = 1200 + 273;  % mean of T_K prior distribution
+priors.T_K_std = 200;  % standard deviation of T_K prior distribution
+
+% mcmc settings:
+% for testing, the following are set to small numbers. they should be increased,
+% which will be obvious when you run this with small numbers...
+settings.max_mcmc_iters = 100; % max iterations for this chain
+settings.mcmc_info_very_N = 10; % print info every N steps
+settings.mcmc_burnin_iters = round(0.2 * settings.max_mcmc_iters); % burn in iterations
+settings.mcmc_jump_std = priors.T_K_std * .05; % the jump magnitude for updating T_K
+settings.mcmc_initial_T_K = 0; % set to 0 to draw initial guess from distribution
+settings.mcmc_initial_guess_jump_std = priors.T_K_std; % jump magnitude for the initial guess
+settings.mcmc_acceptance_sc = 1; % acceptance threshold = sc * rand()
+
+% set the fixed state variables and single anelastic method
+settings.fixed_SVs.P_GPa = 2;
+settings.fixed_SVs.sig_MPa = 0.1;
+settings.fixed_SVs.phi = 0;
+settings.fixed_SVs.dg_um = 0.01 * 1e6;
+settings.fixed_SVs.f = 1. / 50.;
+settings.fit_a_fixed_TK = 1; % 1 to fixed hidden T_K, 0 to draw from distribution about a hidden mean
+settings.anelastic_method = 'eburgers_psp';
+``` 
+
+### A note on `sample_normal`
  
-for `normrnd`
-on octave
-https://wiki.octave.org/Statistics_package
-from octave terminal 
-```commandline
-pkg install -forge statistics
-pkg load statistics
+The functions in `bayes_0d_funcs` include a drop-in replacement for `normrnd`, called 
+`sample_normal` in order to avoid extra dependencies. It's probably a little slow. You
+could instead replace it with calls to `normrnd` in the following ways:
+
+For octave, install the [Statistics package](https://wiki.octave.org/Statistics_package)
+from octave terminal with 
+```matlab
+> pkg install -forge statistics
+> pkg load statistics
 ```
-matlab will require the statistics and ML toolbox `https://www.mathworks.com/products/statistics.html (normally included in educational licenses).
+
+For MATLAB, you'll need to install the [statistics and ML toolbox](https://www.mathworks.com/products/statistics.html) (normally included in educational licenses).
+
+### additional reading 
+
+The following are nice introductions to writing an mcmc sampler from scratch:
 
 * https://exowanderer.medium.com/metropolis-hastings-mcmc-from-scratch-in-python-c21e53c485b7
 * https://twiecki.io/blog/2015/11/10/mcmc-sampling/
-* 
+ 
 
  
