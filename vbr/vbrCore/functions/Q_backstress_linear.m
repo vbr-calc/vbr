f51c700336e19abc2c49ef75f5a51c1442b50332
chavlin <chris.havlin@gmail.com>
2025-08-01 13:54:07 -0700

    field check for sig_MPa sig_dc_MPa

--- vbr/vbrCore/functions/Q_backstress_linear.m
+++ vbr/vbrCore/functions/Q_backstress_linear.m
@@ -8,125 +8,132 @@ function [VBR] = Q_backstress_linear(VBR)
 
     M_GPa = params.M_GPa;
 
-    % pull out state variables 
+    % pull out state variables
     P = VBR.in.SV.P_GPa;
     T = VBR.in.SV.T_K;
-    G_Pa = VBR.out.elastic.anharmonic.Gu; 
-    K_Pa = VBR.out.elastic.anharmonic.Ku;    
-    omega = 2 * pi * VBR.in.SV.f ;     
-    d_nm = VBR.in.SV.dg_um * 1e3; 
+    G_Pa = VBR.out.elastic.anharmonic.Gu;
+    K_Pa = VBR.out.elastic.anharmonic.Ku;
+    omega = 2 * pi * VBR.in.SV.f ;
+    d_nm = VBR.in.SV.dg_um * 1e3;
 
     % initial calculations
-    E_GPa = 9*K_Pa .* G_Pa ./(3*K_Pa+G_Pa) / 1e9; % Pa, Young's modulus     
-    eta1 = visc_calc_backstress_linear(VBR, params); % Pa s, linear viscosity for dislocation glide 
+    E_GPa = 9*K_Pa .* G_Pa ./(3*K_Pa+G_Pa) / 1e9; % Pa, Young's modulus
+    eta1 = visc_calc_backstress_linear(VBR, params); % Pa s, linear viscosity for dislocation glide
 
-    
+
     % allocation of new matrixes
     n_freq = numel(omega);
-    sz = size(VBR.in.SV.T_K);    
-    n_th = numel(VBR.in.SV.T_K); % total elements    
+    sz = size(VBR.in.SV.T_K);
+    n_th = numel(VBR.in.SV.T_K); % total elements
     J1 = proc_add_freq_indeces(zeros(sz),n_freq);
-    J2 = proc_add_freq_indeces(zeros(sz),n_freq);    
-    M = proc_add_freq_indeces(zeros(sz),n_freq);    
+    J2 = proc_add_freq_indeces(zeros(sz),n_freq);
+    M = proc_add_freq_indeces(zeros(sz),n_freq);
     Qinv = proc_add_freq_indeces(zeros(sz),n_freq);
     V = proc_add_freq_indeces(zeros(sz),n_freq);
     valid_f = proc_add_freq_indeces(zeros(sz),n_freq);
-    omega_o = zeros(sz); 
+    omega_o = zeros(sz);
 
-    % initial calulations     
-    sig_MPa = VBR.in.SV.sig_MPa; 
-    sig_p_MPa = params.sig_p_sig_dc_factor * sig_MPa; 
-     
+    % initial calulations
+    if isfield(VBR.in.SV, 'sig_dc_MPa') && ~isfield(VBR.in.SV, 'sig_MPa')
+        sig_MPa = VBR.in.SV.sig_dc_MPa;
+    elseif isfield(VBR.in.SV, 'sig_dc_MPa') && isfield(VBR.in.SV, 'sig_MPa')
+        sig_MPa = VBR.in.SV.sig_MPa;
+    else
+        sig_MPa = VBR.in.SV.sig_MPa;
+    end
+
+    sig_p_MPa = params.sig_p_sig_dc_factor * sig_MPa;
+
     sig_d_MPa = params.Beta .* (G_Pa / 1e6) .* params.burgers_vector_nm ./ d_nm;
-    E_R_Pa = M_GPa .* (sig_p_MPa + sig_d_MPa) ./ sig_p_MPa * 1e9 ;    
-    
+    E_R_Pa = M_GPa .* (sig_p_MPa + sig_d_MPa) ./ sig_p_MPa * 1e9 ;
+
     % the main loop over state variables
     for i_sv = 1:n_th
         eta_i = eta1(i_sv);
-        E_R_i = E_R_Pa(i_sv); 
+        E_R_i = E_R_Pa(i_sv);
         E_U_i = E_GPa(i_sv)*1e9;
         rho_i = VBR.in.SV.rho(i_sv);
-        
-        omega_o(i_sv) = sqrt(E_R_i * (E_R_i + E_U_i)) ./ eta_i / 10; 
+
+        omega_o(i_sv) = sqrt(E_R_i * (E_R_i + E_U_i)) ./ eta_i / 10;
 
         for iw = 1:n_freq
             i_glob = i_sv + (iw - 1) * n_th; % the linear index of the arrays with a frequency index
-            E_star_inv = (1 ./ (E_R_i + eta_i .* omega(iw) .* i) + 1 ./ E_U_i);             
+            E_star_inv = (1 ./ (E_R_i + eta_i .* omega(iw) .* i) + 1 ./ E_U_i);
             E_star_i = 1 ./ E_star_inv;
-            
-            J1(i_glob) = real(1./E_star_i); 
-            J2(i_glob) = imag(1./E_star_i); 
+
+            J1(i_glob) = real(1./E_star_i);
+            J2(i_glob) = imag(1./E_star_i);
             M(i_glob) = norm(E_star_i); % relaxed young's modulus
-            
+
             denom = E_R_i.^2 + E_R_i.*E_U_i + eta_i*eta_i*omega(iw)*omega(iw);
             Qinv(i_glob) = eta_i * E_U_i * omega(iw) ./ (denom);
-            
+
             % assume no attenuation for bulk modulus, calculate relaxed shear modulus
             % from the relaxed young's modulus
             E_i = M(i_glob);
             G_eff = 3 * K_Pa(i_sv) *  E_i / (9 * K_Pa(i_sv) - E_i);
-            % J2_J1_frac=(1+sqrt(1+(J2(i_glob)./J1(i_glob)).^2))/2;            
+            % J2_J1_frac=(1+sqrt(1+(J2(i_glob)./J1(i_glob)).^2))/2;
             V(i_glob) = sqrt(G_eff./ rho_i);
             valid_f(i_glob) = omega(iw) >= omega_o(i_sv);
-        end 
-    end 
-    
+        end
+    end
+
     % put it all int he output structure
-    out_s = struct();    
-    out_s.Qinv = Qinv; 
-    out_s.J1 = J1; 
+    out_s = struct();
+    out_s.Qinv = Qinv;
+    out_s.J1 = J1;
     out_s.J2 = J2;
-    out_s.M = M; 
+    out_s.M = M;
     out_s.V = V;
-    out_s.Vave = Q_aveVoverf(out_s.V, VBR.in.SV.f);  
-    out_s.valid_f = valid_f; 
-    out_s.omega_o = omega_o; 
+    out_s.Vave = Q_aveVoverf(out_s.V, VBR.in.SV.f);
+    out_s.valid_f = valid_f;
+    out_s.omega_o = omega_o;
 
-    out_s.units = Q_method_units(); 
-    out_s.units.omega_o = 'rad/s'; 
-    out_s.units.valid_f = ''; 
-    VBR.out.anelastic.backstress_linear = out_s; 
+    out_s.units = Q_method_units();
+    out_s.units.omega_o = 'rad/s';
+    out_s.units.valid_f = '';
+    VBR.out.anelastic.backstress_linear = out_s;
 
-end 
+end
 
 
 function eta_1 = visc_calc_backstress_linear(VBR, params)
-            
-    Aprime_T = backstress_Aprime(VBR, params); % units: 1 / (s GPa^2)    
-    sig_ref_T_GPa = backstress_sig_ref_GPa(VBR, params);       
 
-    sig_p_GPa = params.sig_p_sig_dc_factor * VBR.in.SV.sig_MPa / 1000; 
-    
+    Aprime_T = backstress_Aprime(VBR, params); % units: 1 / (s GPa^2)
+    sig_ref_T_GPa = backstress_sig_ref_GPa(VBR, params);
+
+    sig_p_GPa = params.sig_p_sig_dc_factor * VBR.in.SV.sig_MPa / 1000;
+
     eta_1 = sig_ref_T_GPa ./ (Aprime_T .* sig_p_GPa.^2);
     eta_1 = eta_1 * 1e9; % Pa s
-    
-end 
+
+end
 
 
 function A_prime_T = backstress_Aprime(VBR, params)
     % units will be: 1 / (s GPa^2)
 
     % parameters for convenience
-    
-    Q = params.Q_J_per_mol; 
-    A = params.A; % MPa−2 s−1   
+
+    Q = params.Q_J_per_mol;
+    A = params.A; % MPa−2 s−1
     R = 8.31446261815324; % J/mol/K
 
-    % state variables 
-    T_K = VBR.in.SV.T_K; 
+    % state variables
+    T_K = VBR.in.SV.T_K;
 
     units_converter =  1e3 * 1e3 ;
     factor = A * units_converter; % MPa-2 s-1 to GPa-2 s -1
-    A_prime_T = factor .* exp(-(Q ./ (R * T_K)));     
-end 
+    A_prime_T = factor .* exp(-(Q ./ (R * T_K)));
+end
 
 function sig_ref_T_GPa = backstress_sig_ref_GPa(VBR, params)
 
-    % parameters for convenience            
+    % parameters for convenience
     R = 8.31446261815324; % J/mol/K
-    BigSigma = params.pierls_barrier_GPa; 
-    dF = params.Q_J_per_mol; 
-    T_K = VBR.in.SV.T_K;     
-    
-    sig_ref_T_GPa = BigSigma .* R .* T_K ./ dF; 
-end 
+    BigSigma = params.pierls_barrier_GPa;
+    dF = params.Q_J_per_mol;
+    T_K = VBR.in.SV.T_K;
+
+    sig_ref_T_GPa = BigSigma .* R .* T_K ./ dF;
+end
