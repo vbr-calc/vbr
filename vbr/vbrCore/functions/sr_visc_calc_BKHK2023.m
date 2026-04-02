05f51e29f9a7e24b11306aa4cda93752f319e457
chavlin <chris.havlin@gmail.com>
2026-04-02 12:49:11 -0500

    set NaN for nonpositive taylor stress, warn once

--- vbr/vbrCore/functions/sr_visc_calc_BKHK2023.m
+++ vbr/vbrCore/functions/sr_visc_calc_BKHK2023.m
@@ -32,12 +32,17 @@ n_SVs = numel(T_K);
 % pre-allocate
 sr_ss = zeros(SV_shape);
 eta = zeros(SV_shape);
+s_rho_ss_mat = zeros(SV_shape);
+s_d_mat = zeros(SV_shape);
 
 % looping over state variables - new version
+warn_stress_threshold = 0;
+warn_taylor_stress_nonpositive = 0;
 for i_SV = 1:n_SVs
     % calculate temperature and grainsize dependent
     % model-specific parameters
     s_d   = params.beta.*VBR.out.elastic.anharmonic.Gu(i_SV).*params.b./d(i_SV); %Pa, Grain-size dependent threshold stress to operate a Frank-Read Source
+    s_d_mat(i_SV) = s_d;
     Rp    = params.A_p*exp(-(params.DeltaF)/8.314/T_K(i_SV))*10^-30; %recovery rate pipe, Pa^-5/s
     Rgb   = params.A_gb*exp(-(params.DeltaF)/8.314/T_K(i_SV))*10^-24; %recovery rate grain boundaries, Pa^-4/s
     A_ltp = params.A*exp(-(params.DeltaF)/8.314/T_K(i_SV))*10^-12; %preexponent in plasticity law, m^2s^-1
@@ -48,10 +53,10 @@ for i_SV = 1:n_SVs
     s_rho_0 = [sig(i_SV)]; %Initial guess of Taylor stress near which to search for negative-slope zero crossing of Taylor stress rate as a function of Taylor stress
     eqn = @(s_rho) (s_rho + s_d)/s_rho*A_ltp*s_rho^2*sinh((sig(i_SV)-s_rho-s_d)/s_ref) - s_rho/params.s_rho_max*abs(A_ltp*s_rho^2*sinh((sig(i_SV)-s_rho-s_d)/s_ref)) - Rp*s_rho^5 - Rgb*s_rho^3*s_d;
     s_rho_ss = fzero(eqn,s_rho_0); %find zero crossing near s_rho_0
-
+    s_rho_ss_mat(i_SV) = s_rho_ss;
     % calculate steady-state strain rate
     sr = A_ltp*s_rho_ss^2*sinh((sig(i_SV)-s_rho_ss-s_d)/s_ref); %calculate steady-state strain rate
-    
+
     % convert strain rate to shear-strain rate
     sr_ss(i_SV) = sr * sqrt(3); %s^-1, plastic shear-strain rate calculated through LTP law of the backstress model
 
@@ -59,18 +64,34 @@ for i_SV = 1:n_SVs
     eta(i_SV) = sig(i_SV) ./ sr_ss(i_SV) / sqrt(3); %Pas, steady-state shear viscosity
 
     if s_d > sig(i_SV) %throw error if grain stress exceeds applied stress and set viscosity and strainrate to NaN
-        warning(['Grain-size dependent threshold stress exceeds deviatoric stress, no steady-state solution for deviatoric stress = ' num2str(sig(i_SV)/1e6) ' MPa and d = ' num2str(d(i_SV)*1e6) ' um. Setting steady-state viscosity and strainrate to NaN. Pick a larger deviatoric stress or larger grain size to resolve this issue.'])
+        warn_stress_threshold = 1;
+        sr_ss(i_SV) = NaN;
+        eta(i_SV)   = NaN;
+    elseif s_rho_ss <= 0
+        warn_taylor_stress_nonpositive = 1;
         sr_ss(i_SV) = NaN;
         eta(i_SV)   = NaN;
     end
 
 end
 
+if warn_stress_threshold == 1
+    msg = 'Grain-size dependent threshold stress exceeds deviatoric stress for some values, BKHK2023 outputs will contain NaN values. Pick a larger deviatoric stress or larger grain size to resolve this issue.';
+    warning([msg])
+end
+
+if warn_taylor_stress_nonpositive == 1
+    msg = 'BKHK2023 calculated non-positive Taylor Stress, outputs will contain NaN values. Pick a larger deviatoric stress or larger grain size to resolve this issue.';
+    warning([msg])
+end
+
 %outputs
 VBR.out.viscous.BKHK2023.gbnp.sr = sr_ss; %s-1, steady-state strain rate
+VBR.out.viscous.BKHK2023.gbnp.sig_rho_ss = s_rho_ss_mat; % taylor stress
+VBR.out.viscous.BKHK2023.gbnp.sig_d = s_d_mat; % s-1, threshold stress to operate a Frank-Read Source
 VBR.out.viscous.BKHK2023.gbnp.eta = eta; %Pas, steady-state viscosity
- VBR.out.viscous.BKHK2023.sr_tot=sr_ss; % total strain rate
- VBR.out.viscous.BKHK2023.eta_total = eta ; % total viscosity
+VBR.out.viscous.BKHK2023.sr_tot=sr_ss; % total strain rate
+VBR.out.viscous.BKHK2023.eta_total = eta ; % total viscosity
 % store total composite strain rate and effective viscosity
 units.sr = "1/s";
 units.eta = "Pa*s";
